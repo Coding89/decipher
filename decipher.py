7169f9685026d6e50435b0575a71520d277f5765
Coding89 <timothy.lefevre1@gmail.com>
2026-06-01 18:49:37 +0100

    Initial commit decipher

---
+++ decipher.py
@@ -0,0 +1,22 @@
+def encrypt(text, shift):
+    text = text.lower()
+    encrypted_text = ""
+    for char in text:
+        if char.islower():
+            encrypted_text += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+def decrypt(text, shift):
+    text = text.lower()
+    decrypted_text = ""
+    for char in text:
+        if char.islower():
+            decrypted_text += chr((ord(char) - shift - 97) % 26 + 97)
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+secret = ""
+print(decrypt(secret, 3))
